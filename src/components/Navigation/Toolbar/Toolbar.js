20145f0f0f05374db9bb758f2e62992c1204115e
arunkumar60 <arunkumar9692@gmail.com>
2020-02-27 20:32:23 +0530

    aaded navigation

--- src/components/Navigation/Toolbar/Toolbar.js
+++ src/components/Navigation/Toolbar/Toolbar.js
@@ -2,12 +2,14 @@ import React from 'react';
 
 import classes from './Toolbar.module.css';
 import Logo from '../../Logo/Logo';
+import NavigationItems from '../NavigationItems/NavigationItems';
 
 const toolbar = () => (
     <header className={classes.Toolbar}>
         <div>Menu</div>
         <Logo />
         <nav>
+            <NavigationItems />
         </nav>
     </header>
 )
