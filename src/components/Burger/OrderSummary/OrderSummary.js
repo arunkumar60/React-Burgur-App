8fe3039898be2ebb9815bc82bd12111a2bfb5688
arunkumar60 <arunkumar9692@gmail.com>
2020-02-27 20:00:22 +0530

    changing the Aux file

--- src/components/Burger/OrderSummary/OrderSummary.js
+++ src/components/Burger/OrderSummary/OrderSummary.js
@@ -1,6 +1,6 @@
 import React from 'react';
 
-import Aux from '../../../hoc/Aux';
+import Aux from '../../../hoc/Auxillary';
 import Button from '../../UI/Button/Button';
 
 const orderSummary = (props) => {
--- src/components/UI/Modal/Modal.js
+++ src/components/UI/Modal/Modal.js
@@ -2,7 +2,7 @@ import React from 'react';
 
 import classes from './Modal.module.css';
 import BackDrop from '../Backdrop/Backdrop';
-import Aux from '../../../hoc/Aux';
+import Aux from '../../../hoc/Auxillary';
 
 const modal = (props) => (
     <Aux>
